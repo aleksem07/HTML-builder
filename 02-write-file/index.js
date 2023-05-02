5ba0a6ba965500de2fc59129a8f63755ab765359
aleksem07 <aleksem07@gmail.com>
2023-05-02 21:59:28 +0300

    feat: add draft tasks 01, 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = process;
+
+fs.writeFile(path.join(__dirname, 'text-file.txt'), '', err => {
+  if (err) throw err;
+});
+
+stdout.write('Пожалуйста, добавьте заметку...\n');
+
+stdin.on('data', data => {
+  if (data.toString().toLowerCase().trim() === 'exit') {
+    console.log('До свидания!');
+    exit();
+  }
+  fs.appendFile(path.join(__dirname, 'text-file.txt'), data, err => {
+    if (err) console.log(err);
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('\nДо свидания!');
+  exit();
+});
