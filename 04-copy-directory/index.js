10934344dc602d3596fe70ffaf39f4ffb1dea773
aleksem07 <aleksem07@gmail.com>
2023-05-03 02:16:28 +0300

    feat: add draft task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathFiles = path.join(__dirname, 'files');
+const pathFilesCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+  if (err) throw err;
+});
+
+fs.readdir(pathFiles, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.copyFile(`${pathFiles}/${file.name}`, `${pathFilesCopy}/${file.name}`, err => {
+        if (err) throw err;
+      });
+    }
+  });
+});
